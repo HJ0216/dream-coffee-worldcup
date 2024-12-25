a48d7449ea5b3d61a17f256ef0f06dda2b7adde0
HJ0216 <6120hj@gmail.com>
2024-12-25 20:10:05 +0900

    🧹Chore: #-- UI 최종 업데이트

--- src/components/Game/ModalOption.js
+++ src/components/Game/ModalOption.js
@@ -38,79 +38,78 @@ function ModalOption({ showModal, closeModal, onFinish, brand }) {
 
   return (
     <ModalBox>
-        <h2 className="title">
-            <img className="title-img"
-                alt="loopy"
-                src={require(`../../image/loopy-img-final.png`)}
-                />
-            <img className="title-text-bg" alt="loopy" src={require("../../image/speech-bubble-left.png")} />
-            <span className="title-text">
-                좀 더 자세히 말해줘!
-            </span>
-        </h2>
         <div className="modal">
-        <div className="modal-content">
+            <div className="modal-content">
+            <h2 className="title">
+                <img className="title-img"
+                    alt="loopy"
+                    src={require(`../../image/loopy-img-final.png`)}
+                    />
+                <img className="title-text-bg" alt="loopy" src={require("../../image/speech-bubble-left.png")} />
+                <span className="title-text">
+                    좀 더 자세히 말해줘!
+                </span>
+            </h2>
+                <div className="button-grid">
+                {/* 커피 vs 논커피 */}
+                <div className="button-pair">
+                    <button
+                        className={`modal-button ${selected.coffee === "커피" ? "selected" : ""}`}
+                        onClick={() => handleSelect("coffee", "커피")}
+                        >
+                        커피
+                    </button>
+                    <button
+                        className={`modal-button ${selected.coffee === "논커피" ? "selected" : ""}`}
+                        onClick={() => handleSelect("coffee", "논커피")}
+                        >
+                        논커피
+                    </button>
+                </div>
 
-            <div className="button-grid">
-            {/* 커피 vs 논커피 */}
-            <div className="button-pair">
-                <button
-                    className={`modal-button ${selected.coffee === "커피" ? "selected" : ""}`}
-                    onClick={() => handleSelect("coffee", "커피")}
+                {/* 아이스 vs 핫 */}
+                <div className="button-pair">
+                    <button
+                        className={`modal-button ${selected.drink === "아이스" ? "selected" : ""}`}
+                        onClick={() => handleSelect("drink", "아이스")}
+                        >
+                        시원하게!
+                    </button>
+                    <button
+                        className={`modal-button ${selected.drink === "핫" ? "selected" : ""}`}
+                        onClick={() => handleSelect("drink", "핫")}
+                        >
+                        따뜻하게!
+                    </button>
+                </div>
+
+                {/* 음료 vs 디저트 */}
+                <div className="button-pair">
+                    <button
+                        className={`modal-button ${selected.dessert === "음료" ? "selected" : ""}`}
+                        onClick={() => handleSelect("dessert", "음료")}
+                        >
+                        음료만!
+                    </button>
+                    <button
+                        className={`modal-button ${selected.dessert === "디저트" ? "selected" : ""}`}
+                        onClick={() => handleSelect("dessert", "디저트")}
+                        >
+                        디저트도!
+                    </button>
+                </div>
+
+                <button 
+                    onClick={handleFinish} 
+                    className={`modal-button complete-btn ${isAllSelected ? 'is-selected' : 'is-not-selected'}`} 
+                    disabled={!isAllSelected}
                     >
-                    커피
+                    끝!
                 </button>
-                <button
-                    className={`modal-button ${selected.coffee === "논커피" ? "selected" : ""}`}
-                    onClick={() => handleSelect("coffee", "논커피")}
-                    >
-                    논커피
-                </button>
+                </div>
+
+                <button onClick={closeModal} className="close-btn">닫기</button>
             </div>
-
-            {/* 아이스 vs 핫 */}
-            <div className="button-pair">
-                <button
-                    className={`modal-button ${selected.drink === "아이스" ? "selected" : ""}`}
-                    onClick={() => handleSelect("drink", "아이스")}
-                    >
-                    시원하게!
-                </button>
-                <button
-                    className={`modal-button ${selected.drink === "핫" ? "selected" : ""}`}
-                    onClick={() => handleSelect("drink", "핫")}
-                    >
-                    따뜻하게!
-                </button>
-            </div>
-
-            {/* 음료 vs 디저트 */}
-            <div className="button-pair">
-                <button
-                    className={`modal-button ${selected.dessert === "음료" ? "selected" : ""}`}
-                    onClick={() => handleSelect("dessert", "음료")}
-                    >
-                    음료만!
-                </button>
-                <button
-                    className={`modal-button ${selected.dessert === "디저트" ? "selected" : ""}`}
-                    onClick={() => handleSelect("dessert", "디저트")}
-                    >
-                    디저트도!
-                </button>
-            </div>
-
-            <button 
-                onClick={handleFinish} 
-                className={`modal-button complete-btn ${isAllSelected ? 'is-selected' : 'is-not-selected'}`} 
-                disabled={!isAllSelected}
-                >
-                끝!
-            </button>
-            </div>
-
-            <button onClick={closeModal} className="close-btn">닫기</button>
-        </div>
         </div>
     </ModalBox>
   );
--- src/components/Game/styleModalOption.js
+++ src/components/Game/styleModalOption.js
@@ -10,13 +10,8 @@ export const ModalBox = styled.div`
 
     .title {
         position: absolute;
-        top: 120px;
-        left: 49%;
-        transform: translateX(-50%);
-        display: flex;
-        align-items: center;
-        gap: 10px;
-        z-index: 101;
+        top: -70px;
+        left: 30px;
     }
 
     /* 제목 이미지 및 텍스트 조정 */
@@ -156,13 +151,8 @@ export const ModalBox = styled.div`
 
     @media screen and (max-width: 768px) {
         .title {
-            top: 150px;
-            left: 50%;
-            transform: translateX(-50%);
-            display: flex;
-            flex-direction: column;
-            align-items: center;
-            gap: 15px;
+            top: -50px;
+            left: 10px;
             width: 100%;
         }
         .title-img {
